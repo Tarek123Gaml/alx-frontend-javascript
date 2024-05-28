bc8b7dc456d4720fc272e25eca88af70e7983d5e
tarek <taga61188@gmail.com>
2024-05-28 13:27:36 +0000

    ES6

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise
-    .all([uploadPhoto(), crearUser()})
-    .then ((res) => {
+  return Promise
+    .all([uploadPhoto(), createUser()})
+    .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
