ab2aff9add37e2f227900d1c55508ecc03c632fa
tarek <taga61188@gmail.com>
2024-06-04 11:15:27 +0000

    set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function(set, startString) {
   const strings = [];
 
+  if (startString === '' || typeof startString !== 'styring') return '';
   set.forEach((s) => {
-    if (typeof(s) === 'string' && s.startswith(startString)) {
+    if (typeof s === 'string' && s.startswith(startString)) {
       strings.push(s.slice(startString.length));
     }
   });
