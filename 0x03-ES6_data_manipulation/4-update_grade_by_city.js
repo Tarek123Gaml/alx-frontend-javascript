3ca18e5686e337fcf6926c0c1f8a49d8dcb8c24d
tarek <taga61188@gmail.com>
2024-06-04 04:06:08 +0000

    data

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,15 +2,15 @@ export default function updateStudentGradeByCity(students, city, newgrades) {
   const cityStudents = students.filter((student) => student.location === city);
   return cityStudents.map((student) => {
     const studentng = newgrades.filter((grade) => grade.studentId === student.id);
-	  if (studentng.length > 0) {
-	    return {
-	      ...student,
-	      grade: studentng[0].grade,
-	    };
-	  }
-	  return {
-	    ...student,
-	    grade: 'N/A',
-	  };
+    if (studentng.length > 0) {
+      return {
+        ...student,
+	grade: studentng[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
   });
 }
